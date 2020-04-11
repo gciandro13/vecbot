daf491b26b5101731d66ffa9956d9740f4de1c17
System Administrator <root@MBP-di-Giovanni.station>
2020-04-11 10:55:55 +0200

    Modificati alcune stampe e inserite nuove voci

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf');
 const assert = require('assert').strict;
-const bot = new Telegraf('TOKEN_BOT');
+const bot = new Telegraf('1162765847:AAH_YizneniaC2ri-gFajnYU94t1weoVf6Q');
 const vec = 'vec';
 const fs = require('fs');
 var lemmaArray = [];
